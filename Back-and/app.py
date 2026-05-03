1ef84a111d341ff532c551299ddb96aefc5cadef
Jardel Maciel <jardel.maciel22@gmail.com>
2026-05-02 23:24:36 -0300

    feat: implementação dos metodos http

---
+++ Back-and/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+ARQUIVO ="dados.json"
+
+def ler_dados():
+    try:
+        with open(ARQUIVO, "r") as f:
+            return json.load(f)
+    except:
+        return []
+
+def salvar_dados(dados):
+    with open(ARQUIVO, "w") as f:
+        json.dump(dados, f, indent=4)
+
+
+@app.route("/produtos", methods=["GET"])
+def listar():
+    return jsonify(ler_dados())
+
+@app.route("/produtos", methods=["POST"])
+def criar():
+    dados = ler_dados()
+    novo = request.json
+    dados.append(novo)
+    salvar_dados(dados)
+    return jsonify(novo)
+
+@app.route("/produtos/<int:index>", methods=["PUT"])
+def atualizar(index):
+    dados = ler_dados()
+    dados[index] = request.json
+    salvar_dados(dados)
+    return jsonify(dados[index])
+
+@app.route("/produtos/<int:index>", methods=["DELETE"])
+def deletar(index):
+    dados = ler_dados()
+    removido = dados.pop(index)
+    salvar_dados(dados)
+    return jsonify(removido)
+
+if __name__ == "__main__":
+    app.run(debug=True)
