2657e65f380a731961d5e97bfcece88803f76c9f
Jardel Maciel <jardel.maciel22@gmail.com>
2026-05-10 02:59:32 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,52 +1,41 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 
 from database.database import criar_tabelas
 
-# ROTAS
 from routes.auth_routes import auth_bp
 from routes.produtos_routes import produtos_bp
-from routes.movimentacoes_routes import movimentacoes_bp
 from routes.dashboard_routes import dashboard_bp
-from routes.xml_importador import xml_bp
+from routes.movimentacoes_routes import movimentacoes_bp
 
 app = Flask(__name__)
 
 CORS(app)
 
 # =========================
-# CRIAR BANCO
+# CRIAR TABELAS
 # =========================
 criar_tabelas()
 
 # =========================
-# REGISTRAR ROTAS
+# ROTAS
 # =========================
 app.register_blueprint(auth_bp)
-
 app.register_blueprint(produtos_bp)
-
+app.register_blueprint(dashboard_bp)
 app.register_blueprint(movimentacoes_bp)
 
-app.register_blueprint(dashboard_bp)
-
-app.register_blueprint(xml_bp)
-
 # =========================
-# ROTA TESTE
+# TESTE API
 # =========================
 @app.route("/")
 def home():
-    return jsonify({
-        "status": "API online"
-    })
+    return {
+        "status": "API ONLINE"
+    }
 
 # =========================
 # START
 # =========================
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=10000,
-        debug=True
-    )
+    app.run(debug=True)
